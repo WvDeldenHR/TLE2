2b49b3d797aecc42351b9622c980d764cc225c39
WvDeldenHR <0960572@hr.nl>
2023-04-26 12:59:22 +0200

    icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,25 @@ function Navbar() {
     return (
         <div className="">
             <nav>
-                
+                <div className="">
+                    <ul className="">
+                        <li className="">
+                            <a className="" href="">
+                                <img className="" src="img/icons/icon_home_002_212427_32x32.svg" alt="Home"></img></a></li>
+                        <li className="">
+                            <a className="" href="">
+                                <img className="" src="img/icons/icon_discover_001_212427_32x32.svg" alt="Ontdekken"></img></a></li>
+                        <li className="">
+                            <a className="" href="">
+                                <img className="" src="img/icons/icon_plus_001_FFFFFF_32x32.svg" alt="Post Maken"></img></a></li>
+                        <li className="">
+                            <a className="" href="">
+                                <img className="" src="img/icons/icon_list_001_212427_32x32.svg" alt="Overzicht"></img></a></li>
+                        <li className="">
+                            <a className="" href="">
+                                <img className="" src="img/icons/icon_user_001_212427_32x32.svg" alt="Instellingen"></img></a></li>
+                    </ul>
+                </div>
             </nav>
         </div>
     );
