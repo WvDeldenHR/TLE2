fd72a3a170e2672a98190af2a21948d7a3d9beee
sena <82781389+senalisa@users.noreply.github.com>
2023-05-16 13:21:50 +0200

    Change the files to work with firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCke_6wBLigt3n6BugUGsG5wIllNQIos4c",
+  authDomain: "tle2-46736.firebaseapp.com",
+  databaseURL: "https://tle2-46736-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tle2-46736",
+  storageBucket: "tle2-46736.appspot.com",
+  messagingSenderId: "721193953439",
+  appId: "1:721193953439:web:679663c2301709b5481aaa",
+  measurementId: "G-2Z421E3JXT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
