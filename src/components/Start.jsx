808d49f674adf52d6a63e67972256a9c3878c316
WvDeldenHR <0960572@hr.nl>
2023-06-26 19:52:21 +0200

    start test

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import start from '../images/start.png'
+// Images
+// import start from '../images/start.png'
 import '../index.css'
 
 export function Start() {
     return ( 
-        <div>
+        <>
+            {/* <div className="sm:mx-auto sm:w-full sm:max-w-sm relative">
+                <img className="mx-auto h-full w-full" src={ start } alt="Start" />
+            </div> */}
 
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm relative">
-                <img
-                    className="mx-auto h-100 w-full"
-                    src={start}
-                    alt="Start"
-                />
-            </div>
+            {/* <div className="">
+                <img className="w-full" src={ start } alt=""/>
+            </div> */}
 
-            <div className="flex justify-center items-center mx-10 mb-5">
+            {/* <div className="flex justify-center items-center mx-10 mb-5">
                 <p className="text-4xl font-bold -mt-10">Samen <br></br> Naar Een <br></br> Betere Buurt</p>
             </div>
-
             <div className="flex justify-center items-center">
                 <p className="mx-10 mb-10 text-sm text-gray-500">Everything works better, together...</p>
             </div>
@@ -43,9 +42,23 @@ export function Start() {
                         INLOGGEN
                     </button>
                 </Link>
-            </div>
+            </div> */}
             
-        
-        </div>
+                <div className="banner | flex flex-col justify-end pb-12 h-screen">
+                    <div className="flex flex-col mb-8 px-8">
+                        <h1 className="pb-2 text-4xl text-dark capitalize font-bold">Samen <br></br>naar een <br></br>betere buurt</h1>
+                        <h2 className="text-sm text-gray-500 font-normal">Everything works better, together...</h2>
+                    </div>
+
+                    <div className="flex flex-col gap-5 px-8">
+                        <Link to="/signup">
+                            <button className="rounded-full px-3 py-5 w-full text-sm text-white uppercase font-semibold bg-green-700 shadow-sm" type="submit">Registreren</button>
+                        </Link>
+                        <Link to="/login">
+                            <button className="rounded-full px-3 py-5 w-full text-sm text-white uppercase font-semibold bg-gray-700 shadow-sm" type="submit">Inloggen</button>
+                        </Link>
+                    </div>
+                </div>
+        </>
     ) 
 }
